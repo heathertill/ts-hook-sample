1fb5231534ccf7a259528f023d320304c60aec08
Heather Till <heather.till@charter.net>
2019-06-01 10:11:21 -0500

    -converted App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,35 +1,70 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 import './scss/app';
 
-export default class App extends React.Component<IAppProps, IAppState> {
+const App: React.SFC<IAppProps> = props => {
 
-    constructor(props: IAppProps) {
-        super(props);
 
-        this.state = { name: null };
-    }
-
-    async componentWillMount() {
+    const [name, setName] = useState<string>('');
+    
+    const getName = async () => {
         let r = await fetch('/api/hello');
         let name = await r.json();
-        this.setState({ name })
+        setName(name);
     }
+    
+    useEffect(() => {
+        getName();
+    }, []);
 
-    render () {
         return (
             <main className="container">
-                <h1 className="covalence-blue">Hello {this.state.name}!</h1>
+                <h1 className="covalence-blue">Hello {name}!</h1>
                 <h2></h2>
             </main>
         )
     }
-}
+
+    export default App
 
 interface IAppProps {
 
 }
 
-interface IAppState {
-    name: string;
-}
+
+// import * as React from 'react';
+
+// import './scss/app';
+
+// export default class App extends React.Component<IAppProps, IAppState> {
+
+//     constructor(props: IAppProps) {
+//         super(props);
+
+//         this.state = { name: null };
+//     }
+
+//     async componentWillMount() {
+//         let r = await fetch('/api/hello');
+//         let name = await r.json();
+//         this.setState({ name })
+//     }
+
+//     render () {
+//         return (
+//             <main className="container">
+//                 <h1 className="covalence-blue">Hello {this.state.name}!</h1>
+//                 <h2></h2>
+//             </main>
+//         )
+//     }
+// }
+
+// interface IAppProps {
+
+// }
+
+// interface IAppState {
+//     name: string;
+// }
